5d8b14ec9ded97b10c946e27625ee69524548946
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-05-21 00:52:35 +0300

    - added header component

---
+++ src/components/header/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './header.scss';
+import { Link } from "react-router-dom";
+const tasksImg = require('../../assets/images/task.png');
+
+function Header() {
+
+
+    return (
+
+
+        <div className="header-container">
+            <div className="header-content max-width">
+                <div class="uk-grid" uk-grid>
+                    <div class="uk-width-1-2 left-side">
+                        <h1>Welcome back<br /> Razvan!</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div className="button-projects">
+                            <Link to="/projects" className="btn-default margin-top">SEE THE PROJECTS</Link>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-2 right-side">
+                        <img src={tasksImg} className="header-img" alt="Tasks" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
