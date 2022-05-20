57fd2f3c970395fb2311a2f22b8a91cf2fccb318
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-05-20 23:41:59 +0300

    - added libraries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import '../node_modules/uikit/dist/css/uikit.css';
+import '../node_modules/uikit/dist/js/uikit-icons.min.js';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+import NavBar from './components/navbar/navbar';
+import Login from './components/auth/login';
+const UIkit = require('uikit')
+const Icons = require('uikit/dist/js/uikit-icons')
+UIkit.use(Icons);
+
+axios.defaults.baseURL = "http://localhost:800/";
+axios.defaults.headers.post['Accept'] = "application/json";
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+
+
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  var retrieveData = localStorage.getItem('auth') ? JSON.parse(localStorage.getItem('auth')) : "";
+  const token = retrieveData.auth_token;
+  config.headers.Authorization = token ? `Beared ${token}` : '';
+  return config;
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <p>TestArea</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={ localStorage.getItem('auth') ? <NavBar /> : <Login />}></Route>
+
+      </Routes>
+    </Router>
   );
 }
 
