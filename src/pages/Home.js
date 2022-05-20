e61b43004bf77c2577bfaade8f3dc20190d9d8fa
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-05-21 00:52:55 +0300

    - added home page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/header/header';
+import NavBar from '../components/navbar/navbar';
+
+
+
+export const Home = () => {
+
+    return (
+        <div className="home-page-container">
+            <NavBar />
+            <Header />
+        </div>
+    )
+
+}
