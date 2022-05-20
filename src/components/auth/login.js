07961f317f5c4c27d6db3d7aa5d4c28204ca35f3
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-05-20 23:42:46 +0300

    -class to className

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -13,30 +13,30 @@ function Login() {
     }
 
     return (
-        <div className="login-container">
-            <div class="uk-section uk-section-muted uk-flex uk-flex-middle uk-animation-fade" uk-height-viewport>
-                <div class="uk-width-1-1">
-                    <div class="uk-container">
-                        <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid>
-                            <div class="uk-width-1-1@m">
-                                <div class="uk-margin uk-width-large uk-margin-auto uk-card uk-card-default uk-card-body uk-box-shadow-large">
-                                    <h3 class="uk-card-title uk-text-center">Back to work?</h3>
+        <div classNameName="login-container">
+            <div className="uk-section uk-section-muted uk-flex uk-flex-middle uk-animation-fade" uk-height-viewport>
+                <div className="uk-width-1-1">
+                    <div className="uk-container">
+                        <div className="uk-grid-margin uk-grid uk-grid-stack" uk-grid>
+                            <div className="uk-width-1-1@m">
+                                <div className="uk-margin uk-width-large uk-margin-auto uk-card uk-card-default uk-card-body uk-box-shadow-large">
+                                    <h3 className="uk-card-title uk-text-center">Back to work?</h3>
                                     <hr></hr>
                                     <form onSubmit={login}>
-                                        <div class="uk-margin">
-                                            <div class="uk-inline uk-width-1-1">
-                                                <span class="uk-form-icon" uk-icon="icon: mail"></span>
-                                                <input class="uk-input uk-form-large" type="text" />
+                                        <div className="uk-margin">
+                                            <div className="uk-inline uk-width-1-1">
+                                                <span className="uk-form-icon" uk-icon="icon: mail"></span>
+                                                <input className="uk-input uk-form-large" type="text" />
                                             </div>
                                         </div>
-                                        <div class="uk-margin">
-                                            <div class="uk-inline uk-width-1-1">
-                                                <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                                                <input class="uk-input uk-form-large" type="password" />
+                                        <div className="uk-margin">
+                                            <div className="uk-inline uk-width-1-1">
+                                                <span className="uk-form-icon" uk-icon="icon: lock"></span>
+                                                <input className="uk-input uk-form-large" type="password" />
                                             </div>
                                         </div>
-                                        <div class="uk-margin">
-                                            <button class="uk-button uk-button-large uk-width-1-1 login-btn">Login</button>
+                                        <div className="uk-margin">
+                                            <button className="uk-button uk-button-large uk-width-1-1 login-btn">Login</button>
                                         </div>
                                     </form>
                                 </div>
