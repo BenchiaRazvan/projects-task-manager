35484766b9a78eb729089969d0f798610f95efa9
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-05-21 00:52:44 +0300

    - logout btn

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './navbar.scss';
 
 function NavBar() {
+    const navigate = useNavigate();
 
+    const logout = () => {
 
+        localStorage.removeItem("auth");
+        navigate('/');
+    }
     return (
 
             <div className="navbar-container">
@@ -16,9 +22,10 @@ function NavBar() {
                         </ul>
                     </div>
                     <div className="uk-navbar-right">
-                        <ul className="uk-navbar-nav uk-visible@s">
-                            <li><a className="uk-text-large" href="https://shubhamjain.co/about/">about</a></li>
-                            <li><a className="uk-text-large" href="https://shubhamjain.co/">blog</a></li>
+                        <ul className="uk-navbar-nav uk-visible@s ul-list">
+                            <li><a className="uk-text-large" href="#0">projects</a></li>
+                            <li><button className="btn-default logout-btn" onClick={logout}>LOGOUT</button></li>
+                            
                         </ul>
                         <a href="#" className="uk-navbar-toggle uk-hidden@s" uk-navbar-toggle-icon uk-toggle="target: #sidenav"></a>
                     </div>
@@ -26,14 +33,12 @@ function NavBar() {
                 <div id="sidenav" uk-offcanvas="flip: true" className="uk-offcanvas">
                     <div className="uk-offcanvas-bar">
                         <ul className="uk-nav">
-                            <li><a className="uk-text-large" href="https://shubhamjain.co/about/">about</a></li>
-                            <li><a className="uk-text-large" href="https://shubhamjain.co/">blog</a></li>
+                            <li><a className="uk-text-large" href="#0">PROJECTS</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
 
- 
 
     )
 }
