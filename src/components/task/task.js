143ce2ba1439aa1ad2b70171b38bd776e1085a2a
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-05-23 19:39:04 +0300

    - created task page

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,18 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './task.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import { toolbarOptions } from '../../utils/informations.js';
 import { faTasks } from '@fortawesome/free-solid-svg-icons';
 import 'react-quill/dist/quill.snow.css';
 import Comments from '../comments/comments';
+import axios from 'axios';
 
 const ReactQuill = require('react-quill'); // CommonJS
 
-function SingleTask() {
+function SingleTask(props) {
+
+    const taskId = useParams().taskId;
+    const [commentDescription, setCommentDescription] = useState("");
+    const [users, setUsers] = useState([]);
+    
+    var projectID = useLocation().state.projID;
+
+
+    const onEditorChange = (e) => {
+
+        setCommentDescription(e)
+    }
+
+    const addComment = async () => {
+
+        const comment = {
+
+            'userid': JSON.parse(localStorage.getItem('auth')).userid,
+            'taskid': taskId,
+            'description': commentDescription
+        }
+
+        await axios.post('/comment', comment).then(res => {
+
+            window.location.reload(false);
+        })
+    }
+
+    const handleSelect = async (e) => {
+
+        const status = e.target.value;
+        const data = {
+
+            'status': e.target.value
+        }
+
+        await axios.patch(`/task/${taskId}`, data).then(res => {
+
+            if(res.status === 200){
+
+                window.location.reload(false);
+            }
+        })
+    }
+    const handleSelectUsers = async (e) => {
+        const userId = e.target.value;
+        
+        const data = {
+            'userid': Number(userId)
+        }
+        console.log(data)
+        await axios.patch(`/task/${taskId}`, data).then(res => {
+
+            if(res.status === 200){
+
+                window.location.reload(false)
+            }
+
+        })
+    }
+
     var modules = {
 
         toolbar: toolbarOptions
     }
+    
+    useEffect(() => {
+
+        axios.get(`/project/assigned/${projectID}`).then(res => {
+
+            if (res.status === 200) {
+
+                setUsers(res.data)
+            }
+        })
+
+
+    }, [])
+
+    const data = props.task;
+
 
 
     return (
@@ -21,19 +101,32 @@ function SingleTask() {
             <div className="single-task-content max-width">
                 <div className="single-task">
                     <div className="space-between border-bottom">
-                        <h1><FontAwesomeIcon icon={faTasks} />Get in touch popup</h1>
+                        <h1><FontAwesomeIcon icon={faTasks} />{data.name}</h1>
                         <div class="uk-form-controls">
-                            <select class="uk-select" id="form-horizontal-select">
-                                <option className="color-option">To do</option>
-                                <option>In progress</option>
-                                <option className="color-option">Done</option>
+                            <select class="uk-select" id="form-horizontal-select" value={data.status} onChange={handleSelect}>
+                                <option className="color-option" value="To Do" >To Do</option>
+                                <option value="In Progress">In Progress</option>
+                                <option className="color-option" value="Done">Done</option>
+                            </select>
+                        </div>
+                        <div class="uk-form-controls">
+                            <select class="uk-select" id="form-horizontal-select" value={data.userid} onChange={handleSelectUsers}>
+                            <option value="0">Unassigned</option>
+                            {
+                                users ? users.map((item, index) => {
+                                    return (
+
+                                        <option className="padding-left-right" value={item.userid}><FontAwesomeIcon icon={faUserAlt}></FontAwesomeIcon>{item.username}</option>
+                                    )                        
+                                }) : ""
+                            }
                             </select>
                         </div>
                     </div>
                     <div className="uk-grid" uk-grid>
                         <div className="uk-width-3-5">
                             <div className="task-description">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p dangerouslySetInnerHTML={{ __html: data.description }}></p>
                             </div>
                             <p className="task-description"></p>
                             <div className="add-comment">
@@ -44,12 +137,14 @@ function SingleTask() {
                                     placeholder={"Add your task description..."}
                                     name="task-description"
                                     id="task-description"
+                                    onChange={onEditorChange}
                                 />
+                                <button type="button" onClick={addComment} className="uk-margin-top btn-default btn-background">Add comment</button>
                             </div>
                         </div>
                         <div className="uk-width-2-5">
-                        <div className="comments-component">
-                                <Comments />
+                            <div className="comments-component">
+                                <Comments comments={data.comments} />
                             </div>
                         </div>
 
